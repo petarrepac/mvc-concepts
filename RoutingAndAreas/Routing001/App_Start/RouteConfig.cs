95b02c3823a1903f821eab21a1907915413a8ae9
Petar Repac <petar.repac@gmail.com>
2013-02-23 17:42:17 +0000

    routing

--- RoutingAndAreas/Routing001/App_Start/RouteConfig.cs
+++ RoutingAndAreas/Routing001/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Routing001
@@ -13,11 +9,16 @@ namespace Routing001
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("NameOfARoute", "{controller}/{action}");
+
+            //var myRoute = new Route("{controller}/{action}", new MvcRouteHandler());
+            //routes.Add("SomeRoute", myRoute);
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
