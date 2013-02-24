7d9ac327c919a64f2b5466d5ff13907c301f9f7a
Petar Repac <petar.repac@gmail.com>
2013-02-24 13:36:11 +0000

    redirect

---
+++ ControllersAndActions/ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello from the DerivedController Index method";
+            return View("MyView");
+        }
+
+        public ActionResult Redirect()
+        {
+            return new RedirectResult("~/Derived/Index");
+        }
+
+        public ActionResult GetDataFromConvenienceProperties()
+        {
+            string userName = User.Identity.Name;
+            string serverName = Server.MachineName;
+            string clientIP = Request.UserHostAddress;
+            DateTime dateStamp = HttpContext.Timestamp;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
